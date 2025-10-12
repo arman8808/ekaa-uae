401c9f0cc2a08f89a5f01de43f31a3f78b7ac88f
arman8808 <armanal3066@gmail.com>
2025-10-12 20:36:43 +0530

    fix the issue raised by aquib

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,63 +1,52 @@
-import React from 'react'
-import HeroSection from '../components/home/HeroSection'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import FAQ from '../components/home/FAQ'
-import BenefitSection from '../components/home/BenefitSection'
-import TrainingPrograms from '../components/home/TrainingPrograms'
-import HowEkaaHelps from '../components/home/HowEkaaHelps'
-import VideoPlayer from '../components/home/VideoPlayer'
-import DecodeComponent from '../components/home/DecodeComponent'
-import Testimonials from '../components/home/Testimonials'
+import React from "react";
+import HeroSection from "../components/home/HeroSection";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import DecodeComponent from "../components/home/DecodeComponent";
+import Testimonials from "../components/home/Testimonials";
 
 function HomePage() {
   return (
     <>
-      <div className="relative w-full min-h-[800px] h-[800px] flex flex-col " style={{ backgroundImage: "url('/Hero Image.svg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div
+        className="relative w-full min-h-[800px] h-[800px] flex flex-col "
+        style={{
+          backgroundImage: "url('/Hero Image.svg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <Header />
         <HeroSection />
       </div>
       <div className="relative">
-        {/* <VideoPlayer /> */}
+        <img
+          src="/hhme/WhatsApp Image 2025-10-07 at 22.33.47.jpeg"
+          alt="hhme"
+          className="object-contain m-4"
+        />
 
-        <img src="/hhme/WhatsApp Image 2025-10-07 at 22.33.47.jpeg" alt="hhme" className='object-contain' />
-        {/* <div className="hidden lg:block absolute top-19 left-1/2 -translate-x-1/2 sm:top-20 sm:left-1/2 md:top-70 md:left-100 lg:top-130 lg:left-1/2 z-[999] pointer-events-none">
-          <img src="/2.2.svg" alt="Leaf" />
-        </div> */}
-        {/* <HowEkaaHelps /> */}
         <div className="relative">
-        {/* <PersonalGuidanceBanner /> */}
-        <div className="hidden lg:flex relative z-10 mt-[-60px] mb-[-30px] justify-center pointer-events-none">
-          <img src="/2.2.svg" alt="Leaf" />
+          <div className="hidden lg:flex relative z-10 mt-[-60px] mb-[-30px] justify-center pointer-events-none">
+            <img src="/2.2.svg" alt="Leaf" />
+          </div>
         </div>
       </div>
-      </div>
-
 
       <div className="relative">
         <DecodeComponent />
-        <div className="hidden lg:block absolute top-[14rem] sm:top-[18rem] left-1/2 -translate-x-1/2 md:top-[27.5rem] lg:top-[27.5rem] z-[999] pointer-events-none">
-          <img src="/2.2.svg" alt="Leaf" />
-        </div>
-        <HowEkaaHelps />
       </div>
 
-      
       {/* <TrainingPrograms/> */}
-
 
       <Testimonials />
 
       <Footer />
     </>
-  )
+  );
 }
 
-export default HomePage
-
-
-
-
+export default HomePage;
 
 //
 
